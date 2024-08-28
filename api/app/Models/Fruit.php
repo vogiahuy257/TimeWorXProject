f2094c72d9c56a54d1fd7d599617bc1dd25161c3
vogiahuy257 <vogiahuy257@gmail.com>
2024-08-28 15:24:54 +0700

    28-08-2024 Commit

---
+++ api/app/Models/Fruit.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fruit extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'fruit_items';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'quantity',
+        'price',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'price' => 'decimal:2',
+    ];
+
+    /**
+     * Get the user that owns the fruit.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
