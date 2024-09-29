5578611f72853a2cf31e121324975f00640d1c5e
vogiahuy257 <vogiahuy257@gmail.com>
2024-09-29 10:39:21 +0700

    29-09-2024  fix task-card interface

--- TimeWorX/resources/js/Components/Dashboard/DashboardProjectView.jsx
+++ TimeWorX/resources/js/Components/Dashboard/DashboardProjectView.jsx
@@ -152,16 +152,18 @@ const DashboardProjectView = ({ auth }) => {
                                     ref={provided.innerRef}
                                     {...provided.droppableProps}
                                 >
+                                    <div className='block-task-header'>
                                     <h1>{columnId}</h1>
-                                    <div className="block-task-list">
-                                        {/* Button to add new task */}
-                                        <PrimaryButton className='task-add-card' onClick={() => handleAddTask(columnId)}>
+                                    {/* Button to add new task */}
+                                    <PrimaryButton className='task-add-card' onClick={() => handleAddTask(columnId)}>
                                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M12 6L12 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
                                                 <path d="M18 12L6 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
                                             </svg>
                                             <p>Add a card</p>
                                         </PrimaryButton>
+                                        </div>
+                                    <div className="block-task-list">
                                             {tasks[columnId].map((task, index) => (
                                                 <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
                                                     {(provided) => (
