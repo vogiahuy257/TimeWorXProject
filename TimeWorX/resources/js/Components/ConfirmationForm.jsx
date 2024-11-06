ac2c2915f44606e71703f5e25f048fd989a1b98c
vogiahuy257 <vogiahuy257@gmail.com>
2024-11-06 18:32:30 +0700

    11-06-2024 create and design more ConfirmationForm to components, Add it on TaskForm

---
+++ TimeWorX/resources/js/Components/ConfirmationForm.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const ConfirmationForm = ({ type,styleToBox, styleToChildren,children, handleConfirm }) => {
+
+    return (
+        <div id="confirmation-form" className={`rounded-2xl flex ${styleToBox}`}>
+
+            <div className="w-1/4 h-full relative">
+                {type == 'help' && 
+                <svg className='icon-confirmation absolute left-8 -translate-y-1/2' id="icon-confirmation" width="78" height="78" viewBox="0 0 78 78" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path className="bg-icon-config" fillRule="evenodd" clipRule="evenodd" d="M45.571 68.4064C46.0378 68.0508 46.579 67.8055 47.1492 67.6667C62.1698 64.0087 73.32 50.4665 73.32 34.32C73.32 15.3656 57.9544 0 39 0C20.0456 0 4.67999 15.3656 4.67999 34.32C4.67999 47.018 11.576 58.1053 21.8272 64.0412C22.5604 64.4658 23.1679 65.0833 23.5394 65.8447L28.2812 75.5654C29.3877 77.8339 32.2928 78.5231 34.3004 76.9935L45.571 68.4064Z" fill="currentColor"/>
+                    <ellipse className="item-icon-config" cx="38.8213" cy="46.8696" rx="2.43478" ry="2.43478" fill="currentColor"/>
+                    <path className="item-icon-config" d="M43.6305 20.4316C41.8412 19.5492 39.8305 19.2173 37.8525 19.4777C35.8746 19.7381 34.0183 20.5791 32.5184 21.8945C31.5445 22.7486 30.7493 23.7776 30.17 24.9218C29.6493 25.9501 30.3284 27.1095 31.4417 27.4078L32.1137 27.5878C33.227 27.8861 34.3449 27.1813 35.0532 26.272C35.2545 26.0137 35.4805 25.7737 35.7291 25.5557C36.5049 24.8753 37.4651 24.4403 38.4881 24.3056C39.5112 24.1709 40.5513 24.3426 41.4767 24.799C42.4022 25.2554 43.1716 25.976 43.6875 26.8696C44.2035 27.7633 44.4429 28.7899 44.3754 29.8196C44.3079 30.8492 43.9366 31.8358 43.3084 32.6545C42.6802 33.4731 41.8234 34.0871 40.8462 34.4188C40.0434 34.6957 39.8273 34.6957 38.9999 34.6957L38.3043 34.6957C37.1517 34.6957 36.2173 35.6301 36.2173 36.7827V40.2609C36.2173 41.4135 37.1517 42.3479 38.3043 42.3479H38.9999C40.1525 42.3479 41.0869 41.4135 41.0869 40.2609C41.0869 39.7503 41.4569 39.3184 41.9474 39.1763C42.0975 39.1329 42.2519 39.0841 42.4115 39.03C44.3006 38.3887 45.9572 37.2016 47.1717 35.6189C48.3861 34.0361 49.104 32.1288 49.2345 30.138C49.365 28.1473 48.9022 26.1626 47.9047 24.4348C46.9072 22.7071 45.4198 21.314 43.6305 20.4316Z" fill="currentColor"/>
+                </svg>
+                }
+                <svg className="h-40 icon-confirmation icon-bottom rounded-bl-2xl" width="140" height="88" viewBox="0 0 140 88" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle className="bg-icon-config" cx="18" cy="40" r="11" fill="currentColor"/>
+                    <circle className="bg-icon-config" cx="77.3911" cy="32.3911" r="8.39107" fill="currentColor"/>
+                    <circle className="bg-icon-config" cx="60.5" cy="76.5" r="11.5" fill="currentColor"/>
+                    <circle className="bg-icon-config" cx="45.1955" cy="18.1955" r="4.19554" fill="currentColor"/>
+                    <circle className="bg-icon-config" cx="22.4714" cy="2.09777" r="2.09777" fill="currentColor"/>
+                    <circle className="bg-icon-config" cx="31.523" cy="142.523" r="76.1453" transform="rotate(-48.1512 31.523 142.523)" fill="currentColor"/>
+                </svg>
+                
+            </div>
+
+            <div className="w-3/4 flex flex-col justify-center h-ful items-start mx-3">
+
+                <p className={`font-normal ${styleToChildren}`}>{children}</p>
+
+                <div className="btn mt-4">
+                    <button
+                        className="px-4 py-1 rounded-md"
+                        onClick={() => handleConfirm(true)}
+                    >
+                        Confirm
+                    </button>
+                    <button
+                        className="ml-4 px-4 py-1 rounded-md"
+                        onClick={() => handleConfirm(false)}
+                    >
+                        Cancel
+                    </button>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default ConfirmationForm;
--- TimeWorX/resources/js/Components/Dashboard/Layouts/Project/TaskForm.jsx
+++ TimeWorX/resources/js/Components/Dashboard/Layouts/Project/TaskForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./css/TaskForm.css";
 import PrimaryButton from "@/Components/PrimaryButton";
-import { toast } from "react-toastify";;
+import { toast } from "react-toastify";
+import ConfirmationForm from "@/Components/ConfirmationForm";
 const TaskForm = ({onClose, user_id,projectId, refreshTasks, task, task_status,project_deadline,is_staff }) => {
     const [taskName, setTaskName] = useState('');
     const [deadline, setDeadLine] = useState('');
@@ -10,6 +11,9 @@ const TaskForm = ({onClose, user_id,projectId, refreshTasks, task, task_status,p
     const [users, setUsers] = useState([]);
     const [isUserBoxVisible, setIsUserBoxVisible] = useState(false);
     const [deadLineError, setDeadLineError] =useState('');
+    // giới hạn task tối đa
+    const [confirmUser, setConfirmUser] = useState(null);
+    const taskLimit = 6;
     
 
 
@@ -157,22 +161,56 @@ const TaskForm = ({onClose, user_id,projectId, refreshTasks, task, task_status,p
     };
 
     const handleUserSelection = (user) => {
+        // Kiểm tra nếu người dùng đã có trong danh sách selectedUsers
         if (selectedUsers.some((u) => u.id === user.id)) {
+            // Nếu đã có, gỡ người dùng ra khỏi selectedUsers
             setSelectedUsers(selectedUsers.filter((u) => u.id !== user.id));
         } else {
-            setSelectedUsers([...selectedUsers, user]);
+            // Nếu chưa có và số lượng task vượt quá giới hạn, hiển thị form xác nhận
+            if (user.active_tasks_count > taskLimit) {
+                setConfirmUser(user);
+            } else{
+                // Nếu không vượt quá giới hạn, thêm trực tiếp vào selectedUsers
+                setSelectedUsers([...selectedUsers, user]);
+            }
         }
+    };
+
+    const handleConfirm = (isConfirmed) => {
+        if (isConfirmed && confirmUser) {
+            setSelectedUsers([...selectedUsers, confirmUser]);
+        }
+        setConfirmUser(null); // Đóng form xác nhận
     };
 
     return (
         <section id="task-form">
-            <div className="task-form-container">
+            <div className="task-form-container relative">
     
                 <form onSubmit={handleSubmit}>
                     <div className="task-form-header">
-                            <h2>{is_staff ? `View ${task?.content}` :(projectId ? (user_id ? (task ? `Edit ${task.name}` : "Create Task") : (task ? `Edit ${task.content}` :  "Create Task")) : "Create Your Personal Plan") }</h2>
-                            <p> {project_deadline ? `Project end date: ${formatDeadline(project_deadline)}` : null}</p>
-                        <div className="form-main">
+                        <div className="flex items-center gap-1 px-1 pt-1">
+                            <svg width="30" height="29" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.4" d="M16.8357 1.91003H7.19569C5.07569 1.91003 3.33569 3.65003 3.33569 5.77003V19.86C3.33569 21.66 4.62569 22.42 6.20569 21.55L11.0857 18.84C11.6057 18.55 12.4457 18.55 12.9557 18.84L17.8357 21.55C19.4157 22.43 20.7057 21.67 20.7057 19.86V5.77003C20.6957 3.65003 18.9657 1.91003 16.8357 1.91003Z" fill="currentColor"/>
+                                <path d="M11.1057 13.2499C10.9157 13.2499 10.7257 13.1799 10.5757 13.0299L9.07565 11.5299C8.78565 11.2399 8.78565 10.7599 9.07565 10.4699C9.36565 10.1799 9.84565 10.1799 10.1357 10.4699L11.1057 11.4399L14.5757 7.96994C14.8657 7.67994 15.3457 7.67994 15.6357 7.96994C15.9257 8.25994 15.9257 8.73994 15.6357 9.02994L11.6357 13.0299C11.4857 13.1799 11.2957 13.2499 11.1057 13.2499Z" fill="currentColor"/>
+                            </svg>
+                            <h2 className="font-medium text-xl">{is_staff ? `View ${task?.content}` :(projectId ? (user_id ? (task ? `Edit ${task.name}` : "Create Task") : (task ? `Edit ${task.content}` :  "Create Task")) : "Create Your Personal Plan") }</h2>
+                        </div>
+                            
+                        <div className="mt-2 flex items-center gap-1 px-1">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16.75 3.56V2C16.75 1.59 16.41 1.25 16 1.25C15.59 1.25 15.25 1.59 15.25 2V3.5H8.74998V2C8.74998 1.59 8.40998 1.25 7.99998 1.25C7.58998 1.25 7.24998 1.59 7.24998 2V3.56C4.54998 3.81 3.23999 5.42 3.03999 7.81C3.01999 8.1 3.25999 8.34 3.53999 8.34H20.46C20.75 8.34 20.99 8.09 20.96 7.81C20.76 5.42 19.45 3.81 16.75 3.56Z" fill="currentColor"/>
+                                <path opacity="0.4" d="M21 10.84V12.58C21 13.19 20.46 13.66 19.86 13.56C19.58 13.52 19.29 13.49 19 13.49C15.97 13.49 13.5 15.96 13.5 18.99C13.5 19.45 13.68 20.09 13.87 20.67C14.09 21.32 13.61 21.99 12.92 21.99H8C4.5 21.99 3 19.99 3 16.99V10.83C3 10.28 3.45 9.82996 4 9.82996H20C20.55 9.83996 21 10.29 21 10.84Z" fill="currentColor"/>
+                                <path d="M19 15C16.79 15 15 16.79 15 19C15 19.75 15.21 20.46 15.58 21.06C16.27 22.22 17.54 23 19 23C20.46 23 21.73 22.22 22.42 21.06C22.79 20.46 23 19.75 23 19C23 16.79 21.21 15 19 15ZM21.07 18.57L18.94 20.54C18.8 20.67 18.61 20.74 18.43 20.74C18.24 20.74 18.05 20.67 17.9 20.52L16.91 19.53C16.62 19.24 16.62 18.76 16.91 18.47C17.2 18.18 17.68 18.18 17.97 18.47L18.45 18.95L20.05 17.47C20.35 17.19 20.83 17.21 21.11 17.51C21.39 17.81 21.37 18.28 21.07 18.57Z" fill="currentColor"/>
+                                <path d="M8.5 15C8.24 15 7.98 14.89 7.79 14.71C7.61 14.52 7.5 14.26 7.5 14C7.5 13.74 7.61 13.48 7.79 13.29C8.02 13.06 8.37 12.95 8.7 13.02C8.76 13.03 8.82 13.05 8.88 13.08C8.94 13.1 9 13.13 9.06 13.17C9.11 13.21 9.16 13.25 9.21 13.29C9.39 13.48 9.5 13.74 9.5 14C9.5 14.26 9.39 14.52 9.21 14.71C9.16 14.75 9.11 14.79 9.06 14.83C9 14.87 8.94 14.9 8.88 14.92C8.82 14.95 8.76 14.97 8.7 14.98C8.63 14.99 8.56 15 8.5 15Z" fill="currentColor"/>
+                                <path d="M12 14.9999C11.74 14.9999 11.48 14.8899 11.29 14.7099C11.11 14.5199 11 14.2599 11 13.9999C11 13.7399 11.11 13.48 11.29 13.29C11.67 12.92 12.34 12.92 12.71 13.29C12.89 13.48 13 13.7399 13 13.9999C13 14.2599 12.89 14.5199 12.71 14.7099C12.52 14.8899 12.26 14.9999 12 14.9999Z" fill="currentColor"/>
+                                <path d="M8.5 18.5C8.24 18.5 7.98 18.39 7.79 18.21C7.61 18.02 7.5 17.76 7.5 17.5C7.5 17.24 7.61 16.98 7.79 16.79C7.89 16.7 7.99 16.63 8.12 16.58C8.49 16.42 8.93 16.51 9.21 16.79C9.39 16.98 9.5 17.24 9.5 17.5C9.5 17.76 9.39 18.02 9.21 18.21C9.02 18.39 8.76 18.5 8.5 18.5Z" fill="currentColor"/>
+                            </svg>
+
+                            <p className="font-normal"> {project_deadline ? `Completion Date: ${formatDeadline(project_deadline)}` : null}</p>
+                        </div>
+                            
+                        <div className="form-main px-1">
                         <div className="form-group task-group">
                                 <label htmlFor="task-name">{projectId ? "Task name:" : "Personal Plan name:"}</label>
                                 <input
@@ -212,7 +250,7 @@ const TaskForm = ({onClose, user_id,projectId, refreshTasks, task, task_status,p
                         </div>
 
                         {projectId && (
-                            <div className="user-list custom-scrollbar">
+                            <div className="user-list custom-scrollbar px-1">
                                 {selectedUsers.map((selectedUser) => (
                                     <div className="user-item" key={selectedUser.id}>
                                         <div className="user-info">
@@ -247,45 +285,64 @@ const TaskForm = ({onClose, user_id,projectId, refreshTasks, task, task_status,p
                     </div>    
 
                     <div className="task-form-button">
-                    <PrimaryButton className="close-button" type="button" onClick={onClose}>
-                        <svg width="24" height="24" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M22.5 7.5L7.5 22.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M7.5 7.5L22.5 22.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg>
-                    </PrimaryButton>
-                    {projectId &&(
-                        <div className="user-box bg-white shadow-md rounded-lg p-4 max-h-80">
-                            <h3 className="text-lg font-semibold text-gray-800 mb-2">Users List</h3>
-                            <ul className="divide-y divide-gray-200 overflow-y-auto max-h-60 scrollbar-hide">
-                                {users.map((user) => (
-                                    <li key={user.id} className="flex justify-between items-center p-3 hover:bg-gray-100 transition duration-300 ease-in-out">
-                                        <div>
-                                            <p className="text-sm font-medium text-gray-800"><strong>Name:</strong> {user.name}</p>
-                                            <p className="text-xs text-gray-600"><strong>Active tasks count:</strong> {user.active_tasks_count}</p>
-                                        </div>
-                                        <input
-                                            type="checkbox"
-                                            value={user.id}
-                                            checked={selectedUsers.some(selectedUser => selectedUser.id === user.id)}
-                                            onChange={() => handleUserSelection(user)}
-                                            disabled={is_staff}
-                                            className="ml-4 h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
-                                        />
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>)
-                        }
-                    {is_staff ? null :
-                        <button type="submit" className="save-button mt-auto">
-                            <h2>{task ? `Save` : "Create"}</h2>
-                        </button>
-                    } 
-                    
+                        <PrimaryButton className="close-button" type="button" onClick={onClose}>
+                            <svg width="24" height="24" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M22.5 7.5L7.5 22.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                <path d="M7.5 7.5L22.5 22.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                            </svg>
+                        </PrimaryButton>
+                        {projectId &&(
+                            <div className="user-box bg-white shadow-md rounded-lg p-4 max-h-80">
+                                <h3 className="text-lg font-semibold ">Users List</h3>
+                                <ul className="overflow-y-auto max-h-60 scrollbar-hide p-2">
+                                    {users.map((user) => (
+                                        <li key={user.id} className="flex justify-between items-center p-3 shadow-md">
+                                            <div>
+                                                <div className="flex items-center gap-1">
+                                                    <svg width="16" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z" fill="currentColor"/>
+                                                        <path d="M11.9999 14.5C6.98991 14.5 2.90991 17.86 2.90991 22C2.90991 22.28 3.12991 22.5 3.40991 22.5H20.5899C20.8699 22.5 21.0899 22.28 21.0899 22C21.0899 17.86 17.0099 14.5 11.9999 14.5Z" fill="currentColor"/>
+                                                    </svg>
+                                                    <p className="text-sm font-medium">{user.name}</p>
+                                                </div>
+                                                <div className="mt-1 flex items-center gap-1">
+                                                <svg width="16" height="14" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M14.85 2H10.15C9.11001 2 8.26001 2.84 8.26001 3.88V4.82C8.26001 5.86 9.10001 6.7 10.14 6.7H14.85C15.89 6.7 16.73 5.86 16.73 4.82V3.88C16.74 2.84 15.89 2 14.85 2Z" fill="currentColor"/>
+                                                    <path d="M17.74 4.81998C17.74 6.40998 16.44 7.70998 14.85 7.70998H10.15C8.56004 7.70998 7.26004 6.40998 7.26004 4.81998C7.26004 4.25998 6.66004 3.90998 6.16004 4.16998C4.75004 4.91998 3.79004 6.40998 3.79004 8.11998V17.53C3.79004 19.99 5.80004 22 8.26004 22H16.74C19.2 22 21.21 19.99 21.21 17.53V8.11998C21.21 6.40998 20.25 4.91998 18.84 4.16998C18.34 3.90998 17.74 4.25998 17.74 4.81998ZM12.88 16.95H8.50004C8.09004 16.95 7.75004 16.61 7.75004 16.2C7.75004 15.79 8.09004 15.45 8.50004 15.45H12.88C13.29 15.45 13.63 15.79 13.63 16.2C13.63 16.61 13.29 16.95 12.88 16.95ZM15.5 12.95H8.50004C8.09004 12.95 7.75004 12.61 7.75004 12.2C7.75004 11.79 8.09004 11.45 8.50004 11.45H15.5C15.91 11.45 16.25 11.79 16.25 12.2C16.25 12.61 15.91 12.95 15.5 12.95Z" fill="currentColor"/>
+                                                </svg>
+                                                <p className="active-task-count text-xs font-light"><strong>Active Tasks:</strong> {user.active_tasks_count}</p>
+                                                </div>
+                                            </div>
+                                            <input
+                                                type="checkbox"
+                                                value={user.id}
+                                                checked={selectedUsers.some(selectedUser => selectedUser.id === user.id)}
+                                                onChange={() => handleUserSelection(user)}
+                                                disabled={is_staff}
+                                                className="ml-4 h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+                                            />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>)
+                            }
+                        {is_staff ? null :
+                            <button type="submit" className="save-button mt-auto">
+                                <h2>{task ? `Save` : "Create"}</h2>
+                            </button>
+                        } 
+                        
                     </div>
-                    
                 </form>
-
+                {confirmUser && (
+                <ConfirmationForm
+                    type={'help'}
+                    styleToBox = {"absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"}
+                    styleToChildren={``}
+                    children={`This ${confirmUser.name} already has too many tasks assigned. Are you sure you want to add them to the project?`}
+                    handleConfirm={handleConfirm}
+                />
+                )}
             </div>
         </section>
     );
