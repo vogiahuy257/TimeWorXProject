b96a9942135f30fcdb94bf437c22d16b6b3882fb
vogiahuy257 <vogiahuy257@gmail.com>
2024-12-05 16:09:08 +0700

    12-05-2024 Fix error of not validating tokens when logging in

--- TimeWorX/routes/web.php
+++ TimeWorX/routes/web.php
@@ -18,6 +18,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function (Request $request) {
 
     $token = $request->session()->get('sanctum_token');
+    
+    // Nếu token không tồn tại, chuyển hướng người dùng đến trang đăng nhập
+    if (!$token) {
+        return redirect()->route('login'); // Đảm bảo có route login
+    }
 
     return Inertia::render('Dashboard',[
         'token' => $token,
